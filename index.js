2f9bbb35ce0c3afe8ac03268725bc78e6446b2ba
Naga Sai Vamsi Chilakalapudi <80962513+vamsi2106@users.noreply.github.com>
2025-02-24 01:39:06 +0530

    updated the swagger documentation

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const swaggerUi = require('swagger-ui-express');
+
+const YAML = require('yaml');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+
+// Load the Swagger YAML file
+const swaggerFilePath = path.join(__dirname, 'swagger.yaml');
+const swaggerFile = fs.readFileSync(swaggerFilePath, 'utf8');
+const swaggerDocument = YAML.parse(swaggerFile);
 
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
@@ -14,6 +24,9 @@ const PORT = 8000;
 app.use(bodyParser.json());
 app.use("/users", userRoutes);
 app.use("/tasks", taskRoutes);
+
+// Serve Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to Task Management" });
